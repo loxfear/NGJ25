377b554ff69589d0b0c3d1ab9bd5ebb970cf7583
Mark <mark@bolverkgames.com>
2025-04-06 14:37:13 +0200

    car sleep

--- NGJ25/Assets/Scripts/Car.cs
+++ NGJ25/Assets/Scripts/Car.cs
@@ -153,6 +153,13 @@ public class Car : MonoBehaviour
             breaking = this.playerControls.Player.HandBreak.ReadValue<float>();
             reset = this.playerControls.Player.Reset.ReadValue<float>();
 
+            if (this.isSleeping)
+            {
+                movement = new Vector2(0, 0);
+                breaking = 0f;
+                reset = 0f;
+            }
+
             if (reset != 0)
             {
                 if(lastCP!=null)
